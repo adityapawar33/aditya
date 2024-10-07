7e5aea1a61c643fed341f3d7f472b4420f020296
adityapawar33 <adityapawar4422@gmail.com>
2024-10-07 17:32:10 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Add{
+    public static void main(String[] args){
+        int N1 = 0 , N2 = 0 ,sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println("Enter 1st Number :  ");
+        N1 = s.nextInt();
+        System.out.println(" ");
+        System.out.println("Enter 2nd Number :  ");
+        N2 = s.nextInt();
+        System.out.println(" ");
+        sum = N1+N2;
+        System.out.println("Addition of "+N1+" and "+N2+" is "+sum);
+        System.out.println(" ");
+        System.out.println("!!!...Thank You...!!!");
+        System.out.println(" ");
+    }
+}
