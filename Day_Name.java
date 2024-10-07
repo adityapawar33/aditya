d2fd398fb59f3527a3905f1a12a4eb2162873d0f
adityapawar33 <adityapawar4422@gmail.com>
2024-10-07 17:39:56 +0530

    Add files via upload

---
+++ Day_Name.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Day_Name{
+    public static void main(String[] args){
+        int Day = 0;
+        System.out.println("\n Enter Day Number to Explor; ");
+        Scanner s = new Scanner(System.in);
+        Day = s.nextInt();
+        switch (Day) {
+            case 1:
+                System.out.println("\n The Day is Monday");
+                break;
+            case 2:
+                System.out.println("\n The Day is Thesday ");
+                break;
+            case 3:
+                System.out.println("\n The Day is Wednesday ");
+                break;
+            case 4:
+                System.out.println("\n The Day is Thursday ");
+                break;
+            case 5:
+                System.out.println("\n The Day is Friday ");
+                break;
+            case 6:
+                System.out.println("\n The Day is Saturday ");
+                break;
+            case 7:
+                System.out.println("\n The Day is Sunday ");
+                break;
+
+            default:
+                System.out.println("\n Day Number Invalid");
+        }
+    }
+}
